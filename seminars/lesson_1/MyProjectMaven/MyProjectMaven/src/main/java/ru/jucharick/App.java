5d3fb598c7dd1c8016f5726dae8880ba90265e94
Julia Ch <juliecha@yandex.ru>
2024-01-03 23:51:23 +0300

    UPD seminar_1_Maven&Gradle

---
+++ seminars/lesson_1/MyProjectMaven/MyProjectMaven/src/main/java/ru/jucharick/App.java
@@ -0,0 +1,15 @@
+package ru.jucharick;
+
+import com.google.common.base.Joiner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
