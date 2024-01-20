fcbe552c782c3b27a4724b832a073c2c2038f823
Julia Ch <juliecha@yandex.ru>
2024-01-20 23:54:02 +0300

    Add hw - lesson_3_Spring Boot

---
+++ homeWork/lesson_3_Spring Boot_Spring_Dependency Injection/src/main/java/ru/jucharick/controllers/UserController.java
@@ -0,0 +1,27 @@
+package ru.jucharick.controllers;
+
+import ru.jucharick.domain.User;
+import ru.jucharick.services.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")//localhost:8080/user
+public class UserController {
+
+
+    @Autowired
+    private RegistrationService service;
+
+    @GetMapping
+    public List<User> userList() { return service.getDataProcessingService().getRepository().getUsers(); }
+
+    @PostMapping("/body")
+    public String userAddFromBody(@RequestBody User user)
+    {
+        service.getDataProcessingService().getRepository().getUsers().add(user);
+        return "User added from body!";
+    }
+}
---
+++ homeWork/lesson_3_Spring Boot_Spring_Dependency Injection/src/main/java/ru/jucharick/services/RegistrationService.java
@@ -0,0 +1,18 @@
+package ru.jucharick.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public DataProcessingService getDataProcessingService() {
+        return dataProcessingService;
+    }
+
+    @Autowired
+    private DataProcessingService dataProcessingService;
+
+    //Поля UserService, NotificationService
+
+    //Метод processRegistration
+}
