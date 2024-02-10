977f0b63201a868aef50c68c5e28e9fb429f42ec
Julia Ch <juliecha@yandex.ru>
2024-02-10 22:59:29 +0300

    Add hW - lesson_7_Spring_Security_Auth_2.0

---
+++ homeWork/lesson_7_Spring_Security_Auth2.0/resource-server/Spring_Auth_2.0/src/test/java/ru/jucharick/Spring_Auth_20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jucharick.Spring_Auth_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
