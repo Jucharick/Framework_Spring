cf2addb62533f1a9ae8a4ab744ad6846605bee32
Julia Ch <juliecha@yandex.ru>
2024-02-18 10:28:50 +0300

    Add hW - lesson_9_Spring_Cloud

---
+++ homeWork/lesson_9_Spring_Cloud_task-user/microservices/tasks/src/test/java/ru/jucharick/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jucharick.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
